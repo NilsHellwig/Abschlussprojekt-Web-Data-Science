22b18ddad665153d8962463a05672af0fa3c0ea6
hen58277 <nils-constantin.hellwig@stud.uni-regensburg.de>
2023-06-10 08:48:18 +0200

    add new file structure

--- 01 data collection /01_crawl_flight_history_departures.R
+++ 01 data collection /01_crawl_flight_history_departures.R
@@ -7,8 +7,13 @@ base_url_departures <- "https://www.flightera.net/en/airport/Munich/EDDM/departu
 # These are the days that are available on Flightera.net for both departures and arrivals
 days_for_crawling <- as.character(seq(as.Date("2017-09-18"), as.Date("2023-05-15"), by = "day"))
 
-# Settings to avoid getting a "Too Many Requests" error 
-requests_per_pause <- 48
+n_hours_to_crawl = 10
+
+# I have determined this parameter "empirically". I always make sure that a maximum of 100 requests are made in 
+# succession in order to avoid a "too many request" error. I also make sure that there is always a 45-minute wait 
+# between these requests. This means that I can use this script to download 
+# flight data without receiving a "too many request" error.
+requests_per_pause <- 100 / n_hours_to_crawl
 pause_duration <- 2800
 
 # Download dataset with flights (as .html files)
@@ -16,7 +21,7 @@ for (i in 1:length(days_for_crawling)) {
   day <- days_for_crawling[i]
   
   # Select two random hours between 0 and 23 and the URLs for the requests
-  hours <- sample(0:23, 2, replace = FALSE)
+  hours <- sample(0:23, n_hours_to_crawl, replace = FALSE)
   urls <- paste0(base_url_departures, day, "%20", sprintf("%02d", hours), "_00?")
   
   for (j in 1:length(hours)) {
@@ -32,8 +37,8 @@ for (i in 1:length(days_for_crawling)) {
     filename <- paste0("datasets/raw_departures_dataset/request_", day, "_", sprintf("%02d", hour), "_00.html")
     writeLines(as.character(parsed_html), filename)
     
-    # Pause for a random amount of time between 7 and 10 seconds
-    Sys.sleep(runif(1, 7, 10))
+    # Pause for 7 minutes
+    Sys.sleep(7)
   }
   
   # Check if the number of requests is a multiple of requests_per_pause.
