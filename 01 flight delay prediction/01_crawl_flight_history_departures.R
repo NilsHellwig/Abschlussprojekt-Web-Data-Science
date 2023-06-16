f45655d28226ce7d7bce7250eda6a428fb7b7b58
hen58277 <nils-constantin.hellwig@stud.uni-regensburg.de>
2023-06-16 10:01:31 +0200

    add comments

--- 01 flight delay prediction/01_crawl_flight_history_departures.R
+++ 01 flight delay prediction/01_crawl_flight_history_departures.R
@@ -1,30 +1,43 @@
 library(httr)
 library(rvest)
 
+# I chose to download flight data from flightera.net. I decided to collect flight data for the period between October 2017 and May 2023. For these months, flight data is available on Flightera for every day. 
+# As downloading takes a long time, I decided to collect flight data for four random hours for each day. 
+# This allowed me to collect around 200,000 flights, which took three full days using this script.
+
 # We first define the URL that serves as the basis for each request. This is either the url for departures or arrivals.
 base_url_departures <- "https://www.flightera.net/en/airport/Munich/EDDM/departure/"
 
 # These are the days that are available on Flightera.net for both departures and arrivals
-days_for_crawling <- as.character(seq(as.Date("2017-09-18"), as.Date("2023-05-15"), by = "day"))
+days_for_crawling <- as.character(seq(as.Date("2017-10-01"), as.Date("2023-05-31"), by = "day"))
 
-n_hours_to_crawl = 10
+n_hours_to_crawl = 4
 
-# I have determined this parameter "empirically". I always make sure that a maximum of 100 requests are made in 
-# succession in order to avoid a "too many request" error. I also make sure that there is always a 45-minute wait 
-# between these requests. This means that I can use this script to download 
-# flight data without receiving a "too many request" error.
-requests_per_pause <- 100 / n_hours_to_crawl
-pause_duration <- 2800
+# A maximum of 99 requests are made in order to avoid a "too many request" error. 
+# I also make sure that there is always a waiting time of at least 45 minutes
+# between these 99 requests. 
+requests_per_pause <- 99
+pause_per_request <- 7
+pause_duration <- 2701
 
-# Download dataset with flights (as .html files)
+n_requests <- 0
+
+# Download flight metadata (as .html files)
 for (i in 1:length(days_for_crawling)) {
   day <- days_for_crawling[i]
   
-  # Select two random hours between 0 and 23 and the URLs for the requests
+  # Select random hours between 0 and 23 and create the URLs for the requests
   hours <- sample(0:23, n_hours_to_crawl, replace = FALSE)
   urls <- paste0(base_url_departures, day, "%20", sprintf("%02d", hours), "_00?")
   
   for (j in 1:length(hours)) {
+    # Check if the number of requests is requests_per_pause.
+    # We need to wait 45 minutes until we can download the next 99 pages.
+    if (n_requests == requests_per_pause) {
+      Sys.sleep(pause_duration)
+      n_requests <- 0
+    }
+    
     hour <- hours[j]
     url <- urls[j]
     
@@ -34,16 +47,12 @@ for (i in 1:length(days_for_crawling)) {
     parsed_html <- read_html(content)
     
     # Write the parsed HTML content to a file
-    filename <- paste0("datasets/raw_departures_dataset/request_", day, "_", sprintf("%02d", hour), "_00.html")
+    filename <- paste0("../datasets/raw_departures_dataset/request_", day, "_", sprintf("%02d", hour), "_00.html")
     writeLines(as.character(parsed_html), filename)
     
-    # Pause for 7 minutes
-    Sys.sleep(7)
-  }
-  
-  # Check if the number of requests is a multiple of requests_per_pause.
-  # We need to wait 45 minutes until we can download the next 99 pages.
-  if (i %% requests_per_pause == 0) {
-    Sys.sleep(pause_duration)
+    # Wait for some seconds until the next request
+    Sys.sleep(pause_per_request)
+    
+    n_requests <- n_requests + 1
   }
 }
--- 01 flight delay prediction/01_crawl_weather_history.R
+++ 01 flight delay prediction/01_crawl_weather_history.R
@@ -1,12 +1,17 @@
 library(httr)
 library(rvest)
 
+# In order to have other features besides the metadata on the flights, I decided to collect historical weather data for the period between October 2017 and May 2023. 
+# Weather data of the departure airport has been considered as features by other studies as well
+# [1] Ding, Y. (2017, August). Predicting flight delay based on multiple linear regression. In IOP conference series: Earth and environmental science (Vol. 81, No. 1, p. 012198). IOP Publishing.
+# [2] Chen, J., & Li, M. (2019). Chained predictions of flight delay using machine learning. In AIAA Scitech 2019 forum (p. 1661).
+
 # We first define the URL that serves as the basis for each request.
 base_url_arrivals <- "https://www.timeanddate.com/weather/germany/munich/historic"
 
-# Since we are only interested in the weather in Munich from September 2017 to May 2023, we will only download the weather data between these two points in time.
+# Since we are only interested in the weather in Munich from October 2017 to May 2023, we will only download the weather data between these two points in time.
 start_year <- 2017
-start_month <- 9
+start_month <- 10
 end_year <- 2023
 end_month <- 5
 
@@ -23,9 +28,9 @@ for (year in start_year:end_year) {
     content <- content(response, as = "text")
     parsed_html <- read_html(content)
     
-    filename <- paste0("datasets/raw_weather_munich_dataset/request_", year_str, "-", month_str, ".html")
+    filename <- paste0("../datasets/raw_weather_munich_dataset/request_", year_str, "-", month_str, ".html")
     
     writeLines(as.character(parsed_html), filename)
-    Sys.sleep(runif(1, 7, 10))
+    Sys.sleep(7)
   }
 }
